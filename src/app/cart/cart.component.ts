0abfecd0770c906c151a21e8a348e8955d3e9d70
diisk <diisk@hotmail.com>
2022-07-09 22:14:50 -0300

    All done

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
 
+import { FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -10,10 +12,26 @@ export class CartComponent {
 
   items = this.cartService.getItems();
 
+  checkoutForm = this.formBuilder.group({
+    name:'',
+    address:''
+  });
+
   constructor(
-    private cartService:CartService
+    private cartService:CartService,
+    private formBuilder:FormBuilder
   ) { }
 
-
+    onSubmit(){
+      this.items = this.cartService.clearCart();
+      const {name,address} = this.checkoutForm.value;
+      this.checkoutForm.reset();
+      if(name&&address){
+        window.alert(`${name} seu pedido foi enviado para ${address}`);
+        return;
+      }
+      window.alert(`Seu pedido foi enviado`);
+      
+    }
   
 }
